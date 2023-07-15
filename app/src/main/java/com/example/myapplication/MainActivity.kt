ac3b59be98864ac8048fb5755c4b8742a6c6848e
Captain Storm <CaptainStorm21@users.noreply.github.com>
2023-07-15 11:33:06 -0700

    lecture 11 removed inside of the setContent

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,9 +3,6 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -16,31 +13,38 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            Greeting(name = "William")
         }
     }
 }
 
+
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Good Morning, $name!",
         modifier = modifier
     )
 }
 
 @Preview(showBackground = true)
 @Composable
+        /*
+        Composable functions are the fundamental building blocks of an application built with Compose.
+        Composable can be applied to a function or lambda to indicate that the function/lambda can be used
+        as part of a
+        composition to describe a transformation from application data into a tree or hierarchy.
+        Annotating a function or expression with Composable changes the type of that function or expression.
+        For example, Composable functions can only ever be called from within another Composable function.
+         A useful mental model for Composable functions is that an implicit "composable context"
+         is passed into a Composable function, and is done so implicitly when it is called from
+         within another Composable function.
+        This "context" can be used to store information from previous executions of the function
+        that happened at the same logical point of the tree.
+        */
+
 fun GreetingPreview() {
     MyApplicationTheme {
-        Greeting("Android")
+        Greeting("William")
     }
 }
